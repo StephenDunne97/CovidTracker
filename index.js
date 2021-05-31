a7e9da53767d9bb6b37d128efe55a9a1cfca4752
StephenDunne97 <dunne134@gmail.com>
2021-05-31 14:00:22 +0100

    Few bits

--- index.js
+++ index.js
@@ -1,39 +1,21 @@
-var requestOptions = {
+let requestOptions = {
     method: 'GET',
     redirect: 'follow'
 };
 
-function populateCountryList() { // Fxn to get list of countries
+// Function to dynamically populate the countries list
+async function populateCountryList() {
     fetch("https://api.covid19api.com/countries", requestOptions)
     .then(response => response.json())
     .then(countries => extractCountryData(countries)) // Countries are returned from API as an array of JSON objects
-    .catch(error => console.log('error', error));
+    .catch(error => console.log('Failed to populate countries list.', error));
 }
 
-function initPopGlobal() {
-    console.log("init");
-    // Get global summary cases
-    fetch(`https://api.covid19api.com/summary`, requestOptions)
-    .then(response => response.json())
-    .then(summaryData => popGlobal(summaryData)) // Countries are returned from API as an array of JSON objects
-    .catch(error => console.log('error', error));
-}
-
-function popGlobal(summaryData){
-    var summary = summaryData.Global;
-    const countryDataDiv = document.querySelector('#country-data-centre');
-    countryDataDiv.innerHTML ="";
-    const countryDataElement = document.createElement(`p`); // Create a line item element
-    countryDataElement.classList.add("p-data"); // Apply class for styling
-    countryDataElement.innerText = `Global cases: ${summary.TotalConfirmed}`; // Set text to country name
-    countryDataDiv.append(countryDataElement);
-    
-}
-
-function extractCountryData(countries){ // Fxn to parse country array 
+// Dynamically populate the 'Countries' list
+function extractCountryData(countries){
     const countryDiv = document.querySelector('#country-list');
-    countries.sort(function(a,b){ // Sorts in alphabetical order
-        var x = a.Country < b.Country? -1:1; 
+    countries.sort(function(a,b){ // Sorts countries in alphabetical order
+        let x = a.Country < b.Country? -1:1; 
         return x; 
     });
     countries.forEach(country => {
@@ -41,89 +23,173 @@ function extractCountryData(countries){ // Fxn to parse country array
         countryElement.classList.add("list-group-item"); // Apply class for styling
         countryElement.innerText = `${country.Country}`; // Set text to country name
         countryElement.id = `${country.Slug}`;
-        
         countryDiv.append(countryElement);
     });
 }
 
-function getListItemSlug(){ // Returns the name and slug of the list-item 
+// Function to get global COVID-19 Stats
+async function initPopGlobal() {
+    let globalURL = `https://api.covid19api.com/summary`;
+    // Get global summary cases
+    fetch(globalURL, requestOptions)
+    .then(response => response.json())
+    .then(summaryData => popGlobal(summaryData)) // Countries are returned from API as an array of JSON objects
+    .catch(error => console.log('Failed to populate global stats.', error));
+}
+
+// Function to populate Global Stats
+function popGlobal(summaryData){
+    let summary = summaryData.Global; // Global = Object retrieved from API call
+    const countryDataDivHeader = document.querySelector('#country-data-header');
+    const countryDataDiv = document.querySelector('#country-data-centre');
+    const countryDataRecovered = document.querySelector('#country-data-breakdown-recovered');
+    const countryDataDeaths = document.querySelector('#country-data-breakdown-deaths');
+    const countryDataActive = document.querySelector('#country-data-breakdown-active');
+
+    // Populate Header
+    countryDataDivHeader.innerHTML ="";
+    const countryHeaderElement = document.createElement(`p`); // Create a line item element
+    countryHeaderElement.classList.add("p-data"); // Apply class for styling
+    countryHeaderElement.innerText = `COVID-19 Cases Global Overview`; // Set text to country name
+    countryDataDivHeader.append(countryHeaderElement);
+    
+    // Populate Centre
+    countryDataDiv.innerHTML ="";
+    const countryDataElement = document.createElement(`p`); // Create a line item element
+    countryDataElement.classList.add("p-data"); // Apply class for styling
+    countryDataElement.innerText = `Global cases: ${summary.TotalConfirmed}`; // Set text to country name
+    countryDataDiv.append(countryDataElement);
+    
+    // Populate Recovered
+    countryDataRecovered.innerHTML ="";
+    const countryDataElementRecovered = document.createElement('p');
+    countryDataElementRecovered.classList.add("p-sub-data"); // Apply class for styling
+    countryDataElementRecovered.innerText = `Global recovered: ${summary.TotalRecovered}`; // Set text to country name
+    countryDataRecovered.append(countryDataElementRecovered);
+
+    // Populate Deaths
+    countryDataDeaths.innerHTML ="";
+    const countryDataElementDeaths = document.createElement('p');
+    countryDataElementDeaths.classList.add("p-sub-data"); // Apply class for styling
+    countryDataElementDeaths.innerText = `Global Deaths: ${summary.TotalDeaths}`; // Set text to country name
+    countryDataDeaths.append(countryDataElementDeaths);
+
+    // Populate Active
+    let active = summary.TotalConfirmed - summary.TotalRecovered - summary.TotalDeaths;
+    countryDataActive.innerHTML ="";
+    const countryDataElementActive = document.createElement('p');
+    countryDataElementActive.classList.add("p-sub-data"); // Apply class for styling
+    countryDataElementActive.innerText = `Global Active: ${active}`; // Set text to country name
+    countryDataActive.append(countryDataElementActive);
+}
+
+// Function that gets the name and slug of the clicked list-item
+function getListItemSlug(){ 
     document.getElementById("country-list").addEventListener("click", function(e){
         if(e.target && e.target.nodeName == "LI"){
-            var countrySlug = e.target.id;
-            var countryName = e.target.innerText;
-            console.log("List item", countrySlug, "was clicked.");
-            console.log(countryName);
+            let countrySlug = e.target.id;
+            let countryName = e.target.innerText;
             populateDataButton(countrySlug, countryName);
         }
     });
 }
 
-function populateDataButton(countrySlug, countryName){ // Changes button data 
+// Alters appearance of button based on the country selected
+function populateDataButton(countrySlug, countryName){
     const countryDataButton = document.querySelector('#get-country-data');
     countryDataButton.innerText = (`Get ${countryName} data`);
     countryDataButton.setAttribute("slug",countrySlug) // Slug is attribute used to change API call
 }
 
 function getCountryData(slug){
-    console.log("SLUG:", slug);
+    let confirmedURL = `https://api.covid19api.com/total/country/${slug}/status/confirmed`;
+    let recoveredURL = `https://api.covid19api.com/total/country/${slug}/status/recovered`;
+    let deathsURL = `https://api.covid19api.com/total/country/${slug}/status/deaths`;
+
     // Get confirmed cases
-    fetch(`https://api.covid19api.com/total/country/${slug}/status/confirmed`, requestOptions)
+    fetch(confirmedURL, requestOptions)
     .then(response => response.json())
     .then(countryData => formatCountryData(countryData, slug)) // Countries are returned from API as an array of JSON objects
-    .catch(error => console.log('error', error));
-
+    .catch(error => console.log('Failed to load confirmed cases.', error));
+  
     // Get recovered cases 
-    /*
-    fetch(`https://api.covid19api.com/total/country/${slug}/status/recovered`, requestOptions)
+    fetch(recoveredURL, requestOptions)
     .then(response => response.json())
-    .then(countryData => formatCountryData(countryData)) // Countries are returned from API as an array of JSON objects
-    .catch(error => console.log('error', error));
+    .then(recoveredData => populateBreakdownRecovered(recoveredData, slug)) // Countries are returned from API as an array of JSON objects
+    .catch(error => console.log('Failed to load recovered cases.', error));
 
     // Get death cases 
-    fetch(`https://api.covid19api.com/total/country/${slug}/status/deaths`, requestOptions)
+    fetch(deathsURL, requestOptions)
     .then(response => response.json())
-    .then(countryData => formatCountryData(countryData)) // Countries are returned from API as an array of JSON objects
-    .catch(error => console.log('error', error));
-    */
+    .then(deathsData => populateBreakdownDeaths(deathsData)) // Countries are returned from API as an array of JSON objects
+    .catch(error => console.log('Failed to load deaths.', error));
 }
 
+// Function to present the confirmed cases of a country 
 function formatCountryData(countryData, slug){
-    console.log("Ye boi");
-    var lastItem = countryData.slice(countryData.length - 1);
-    console.log(lastItem);
+    let lastItem = countryData.slice(countryData.length - 1); // Gets the 2nd last item to show yesterdays stats instead of todays
     const countryDataDivHeader = document.querySelector('#country-data-header');
     const countryDataDivCentre = document.querySelector('#country-data-centre');
     const countryDataDivFooter = document.querySelector('#country-data-footer');
+    const countryDataActive = document.querySelector('#country-data-breakdown-active');
     countryDataDivHeader.innerHTML ="";
     countryDataDivCentre.innerHTML ="";
     countryDataDivFooter.innerHTML ="";
-    if (lastItem.length != 0){
+    countryDataActive.innerHTML ="";
+    if (lastItem.length != 0){ // If there is data for this country 
         lastItem.forEach(entry => {
             // Populate Header
-            const countryHeaderElement = document.createElement(`p`); // Create a line item element
+            const countryHeaderElement = document.createElement(`p`); // Create a paragraph element
             countryHeaderElement.classList.add("p-data"); // Apply class for styling
-            countryHeaderElement.innerText = `Presenting data for: ${slug}`; // Set text to country name
-            countryDataDivHeader.append(countryHeaderElement);
+            countryHeaderElement.innerText = `COVID-19 case overview in ${slug}`; // Set text to country name
+            countryDataDivHeader.append(countryHeaderElement); // Add element to div
             
             // Populate Centre 
-            const countryDataElement = document.createElement(`p`); // Create a line item element
-            countryDataElement.classList.add("p-data"); // Apply class for styling
-            countryDataElement.innerText = `Confirmed cases: ${entry.Cases}`; // Set text to country name
+            const countryDataElement = document.createElement(`p`); 
+            countryDataElement.classList.add("p-data"); 
+            countryDataElement.innerText = `Confirmed cases: ${entry.Cases}`; 
             countryDataDivCentre.append(countryDataElement);
 
             // Populate Footer
-            var date = new Date();
+            let date = new Date();
             date.setDate(date.getDate() - 1);
-            console.log(date);
-            const countryFooterElement = document.createElement(`p`); // Create a line item element
-            countryFooterElement.classList.add("p-data"); // Apply class for styling
-            countryFooterElement.innerText = `Last updated: ${date.toDateString().slice(0,10)}`; // Set text to country name
+            const countryFooterElement = document.createElement(`p`);
+            countryFooterElement.classList.add("p-date");
+            countryFooterElement.innerText = `Last update: ${date.toDateString().slice(0,10)}`;
             countryDataDivFooter.append(countryFooterElement);
         });    
     }
-    else{
+    else{ // If there is no data for this country 
         const countryDataElement = document.createElement(`p`); // Create a line item element
         countryDataElement.classList.add("p"); // Apply class for styling
-        countryDataDiv.append("No data available for this country.");
+        countryDataDivHeader.append("No data available for this country.");
     }
+}
+
+// Function to populate the Recovered Cases div
+function populateBreakdownRecovered(recoveredData, slug){
+    let lastItem = recoveredData.slice(recoveredData.length - 1);
+    const countryDataDivBreakdown = document.querySelector('#country-data-breakdown-recovered');
+    countryDataDivBreakdown.innerHTML ="";
+    lastItem.forEach(entry => {
+        // Populate
+        const countryBreakdownElement = document.createElement(`p`); // Create a line item element
+        countryBreakdownElement.classList.add("p-sub-data"); // Apply class for styling
+        countryBreakdownElement.innerText = `Recovered cases: ${entry.Cases}`; // Set text to country name
+        countryDataDivBreakdown.append(countryBreakdownElement);
+    });
+}
+
+// Function to populate the Recorded Deaths div
+function populateBreakdownDeaths(deathsData, slug){
+    let lastItem = deathsData.slice(deathsData.length - 1);
+    const countryDataDivBreakdown = document.querySelector('#country-data-breakdown-deaths');
+    countryDataDivBreakdown.innerHTML ="";
+    lastItem.forEach(entry => {
+        // Populate
+        const countryBreakdownElement = document.createElement(`p`); // Create a line item element
+        countryBreakdownElement.classList.add("p-sub-data"); // Apply class for styling
+        countryBreakdownElement.innerText = `Recorded Deaths: ${entry.Cases}`; // Set text to country name
+        countryDataDivBreakdown.append(countryBreakdownElement);
+    });
 }
